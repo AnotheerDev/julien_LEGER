6a5f02d8ecf8921616c38b153208d9d69e95999e
AnotheerDev <leger.julien94@gmail.com>
2023-04-13 22:17:38 +0200

    reste à coder la rendue de monnaie

--- ALGOPHP/exo10.php
+++ ALGOPHP/exo10.php
@@ -7,3 +7,17 @@ affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 
 <h2>Résultat</2>
 
 <?php
+
+$aPayer = 152;
+$verse = 200;
+$resteAPayer = $verse - $aPayer;
+
+
+
+
+
+echo "Montant à payer : ".$aPayer." €<br>";
+echo "Montant versé : ".$verse." €<br>";
+echo "Reste à payer : ".$resteAPayer." €<br>";
+echo "****************************************<br>";
+echo "Rendue de monnaie : <br>";
