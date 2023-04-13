c0bd3c53349257f06455b84fab2d112577cf6d7c
AnotheerDev <leger.julien94@gmail.com>
2023-04-13 15:29:34 +0200

    fin

--- ALGOPHP/exo7.php
+++ ALGOPHP/exo7.php
@@ -11,13 +11,13 @@ $age = 10;
 
 if ($age >=6 && $age <=7) {
     echo "L'enfant appartient à la catégorie « Poussin » ";
-} elseif ($age >=8 && $age <=9) {
+}   elseif ($age >=8 && $age <=9) {
     echo "L'enfant appartient à la catégorie « Pupille » ";
-} elseif ($age >=10 && $age <=11) {
+}   elseif ($age >=10 && $age <=11) {
     echo "L'enfant appartient à la catégorie « Minime » ";
-}elseif ($age >=12) {
+}   elseif ($age >=12) {
     echo "L'enfant appartient à la catégorie « Cadet » ";
-}elseif ($age <=6) {
+}   elseif ($age <=6) {
     echo "Il n'y a pas de catégorie pour cet enfant";
 }
 
