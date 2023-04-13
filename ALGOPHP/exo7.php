5a5a5d7fd2b7e5c4484f1633affb18c23181ecdb
AnotheerDev <leger.julien94@gmail.com>
2023-04-13 15:28:19 +0200

    fin exo 7

--- ALGOPHP/exo7.php
+++ ALGOPHP/exo7.php
@@ -2,8 +2,22 @@
 
 <p>Ecrire un algorithme permettant de renvoyer la catégorie d’un enfant en fonction de son âge :</p>
 
-<h2>Résultat</2>
+<h2>Résultat</h2>
 
 <?php
 
+$age = 10; 
+
+
+if ($age >=6 && $age <=7) {
+    echo "L'enfant appartient à la catégorie « Poussin » ";
+} elseif ($age >=8 && $age <=9) {
+    echo "L'enfant appartient à la catégorie « Pupille » ";
+} elseif ($age >=10 && $age <=11) {
+    echo "L'enfant appartient à la catégorie « Minime » ";
+}elseif ($age >=12) {
+    echo "L'enfant appartient à la catégorie « Cadet » ";
+}elseif ($age <=6) {
+    echo "Il n'y a pas de catégorie pour cet enfant";
+}
 
