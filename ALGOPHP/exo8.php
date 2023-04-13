0eb5f5b4e8811babe0e65fe2c8c54177d1e5d0ab
AnotheerDev <leger.julien94@gmail.com>
2023-04-13 16:27:13 +0200

    rectification para

--- ALGOPHP/exo8.php
+++ ALGOPHP/exo8.php
@@ -1,6 +1,7 @@
 <h1>Exercice 8</1>
 
-<p>Ecrire un algorithme permettant de renvoyer la catégorie d’un enfant en fonction de son âge :</p>
+<p>Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre sous la 
+forme : </p>
 
 <h2>Résultat</h2>
 
