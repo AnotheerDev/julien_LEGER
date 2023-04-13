2da40171d3f58f9535f5dce430bbe4731cb919fd
AnotheerDev <leger.julien94@gmail.com>
2023-04-13 15:46:03 +0200

    correction exo7

--- ALGOPHP/exo7.php
+++ ALGOPHP/exo7.php
@@ -9,15 +9,29 @@
 $age = 10; 
 
 
+// if ($age >=6 && $age <=7) {
+//     echo "L'enfant appartient à la catégorie « Poussin » ";
+// }   elseif ($age >=8 && $age <=9) {
+//     echo "L'enfant appartient à la catégorie « Pupille » ";
+// }   elseif ($age >=10 && $age <=11) {
+//     echo "L'enfant appartient à la catégorie « Minime » ";
+// }   elseif ($age >=12) {
+//     echo "L'enfant appartient à la catégorie « Cadet » ";
+// }   elseif ($age <=6) {
+//     echo "Il n'y a pas de catégorie pour cet enfant";
+// }
+// ça fonctionne mais le résultat démandé était pas le bon je laisse ça au cas où
+
 if ($age >=6 && $age <=7) {
-    echo "L'enfant appartient à la catégorie « Poussin » ";
+    $cat = "Poussin";
 }   elseif ($age >=8 && $age <=9) {
-    echo "L'enfant appartient à la catégorie « Pupille » ";
+        $cat = "Pupille";
 }   elseif ($age >=10 && $age <=11) {
-    echo "L'enfant appartient à la catégorie « Minime » ";
+        $cat = "Minime";
 }   elseif ($age >=12) {
-    echo "L'enfant appartient à la catégorie « Cadet » ";
+        $cat = "Cadet";
 }   elseif ($age <=6) {
     echo "Il n'y a pas de catégorie pour cet enfant";
 }
 
+echo "L’enfant qui a $age ans appartient à la catégorie « $cat »";
