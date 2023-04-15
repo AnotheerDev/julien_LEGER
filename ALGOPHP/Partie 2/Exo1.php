f3c10fd8b8ae9611942892c6b0f918bbcc7e3cae
AnotheerDev <leger.julien94@gmail.com>
2023-04-15 23:44:31 +0200

    debut exo 1 p2

---
+++ ALGOPHP/Partie 2/Exo1.php
@@ -0,0 +1,10 @@
+<h1>Exercice 1</1>
+
+<p>Créer une fonction personnalisée convertirMajRouge() permettant de transformer une chaîne de caractère passée en argument en majuscules et en rouge.Vous devrez appeler la fonction comme suit: convertirMajRouge($texte); </p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$texte = "mon texte en paramètre"
+
