9352ac7caa710bb863c7f02af8c15434ab0359aa
AnotheerDev <leger.julien94@gmail.com>
2023-04-16 01:26:13 +0200

    fin exo 3

---
+++ ALGOPHP/Part2exo3.php
@@ -0,0 +1,9 @@
+<h1>Exercice 3</1>
+
+<p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir dans un nouvel onglet (_blank).</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+echo '<a href="http://www.elan-formation.fr" target="_blank">Ptit tour sur le site</a>';
