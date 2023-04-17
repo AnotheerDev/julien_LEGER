5e1fbcf23a9962cbb4bc0c6037b2571b8328c62a
AnotheerDev <leger.julien94@gmail.com>
2023-04-17 14:20:03 +0200

    Exo 11 ajout  fonction mais la date reste en EN.

--- ALGOPHP/Part2exo11.php
+++ ALGOPHP/Part2exo11.php
@@ -1,6 +1,6 @@
 <h1>Exercice 11</1>
 
-<p>crire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d’une 
+<p>Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d’une 
 chaîne de caractère représentant une date.
 Exemple :
 formaterDateFr("2018-02-23");</p>
@@ -10,3 +10,11 @@ formaterDateFr("2018-02-23");</p>
 
 <?php
 
+$date = "2018-02-23";
+echo ecrireDate($date);
+
+function ecrireDate($date) {
+    setlocale(LC_TIME, 'fr_FR');
+    $dateTrans = DateTime::createFromFormat('Y-m-d', $date);
+    return $dateTrans->format('l j F Y');
+}
