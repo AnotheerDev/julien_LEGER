9cabd1d64d39f66773bb92120db9990b63716d3f
AnotheerDev <leger.julien94@gmail.com>
2023-04-17 15:23:00 +0200

    1st

--- ALGOPHP/Part2exo13.php
+++ ALGOPHP/Part2exo13.php
@@ -1,9 +1,16 @@
 <h1>Exercice 13</1>
 
-<p>La fonction var_dump($variable) permet d’afficher les informations d’une variable.
-Soit le tableau suivant : 
-$tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));
-A l’aide d’une boucle, afficher les informations des variables contenues dans le tableau</p>
+<p>Créer une classe Voiture possédant les propriétés suivantes : marque, modèle, nbPortes et 
+vitesseActuelle ainsi que les méthodes demarrer( ), accelerer( ) et stopper( ) en plus 
+des accesseurs (get) et mutateurs (set) traditionnels. La vitesse initiale de chaque véhicule 
+instancié est de 0. Une méthode personnalisée pourra afficher toutes les informations d’un 
+véhicule. 
+v1 ➔ "Peugeot","408",5
+v2 ➔ "Citroën","C4",3
+Coder l’ensemble des méthodes, accesseurs et mutateurs de la classe tout en réalisant des jeux de 
+tests pour vérifier la cohérence de la classe Voiture. Vous devez afficher les tests et les éléments 
+suivants :
+</p>
 
 <h2>Résultat</h2>
 
